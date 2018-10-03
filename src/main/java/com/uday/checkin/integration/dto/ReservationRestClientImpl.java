21a8aaeec9a882510c42fc5e3368b4be43cbef25
udayk <udaykumar1113@gmail.com>
2018-10-03 09:01:28 -0400

    Added REST template to make GET and POST requests to flight-reservation microservice

--- src/main/java/com/uday/checkin/integration/dto/ReservationRestClientImpl.java
+++ src/main/java/com/uday/checkin/integration/dto/ReservationRestClientImpl.java
@@ -1,15 +1,27 @@
 package com.uday.checkin.integration.dto;
 
 import com.uday.checkin.integration.ReservationRestClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+@Component
 public class ReservationRestClientImpl implements ReservationRestClient {
+
+    private static final String RESERVATION_REST_URL="localhost:8080/getReservation/";
+
     @Override
     public Reservation findReservation(Long id) {
-        return null;
+
+        RestTemplate restTemplate=new RestTemplate();
+        Reservation reservation=restTemplate.getForObject(RESERVATION_REST_URL+id,Reservation.class);
+        return reservation;
     }
 
     @Override
     public Reservation updateReservation(ReservationRequest request) {
-        return null;
+         RestTemplate restTemplate=new RestTemplate();
+         Reservation reservation=restTemplate.postForObject("localhost:8080/updateReservation",
+                 request,Reservation.class);
+        return reservation;
     }
 }
