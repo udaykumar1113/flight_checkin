82a90ddf502a963289e9d4dde89ecd183c345cc8
udayk <udaykumar1113@gmail.com>
2018-10-04 09:40:54 -0400

    Added update reservation UI to checkin with bags

--- src/main/java/com/uday/checkin/controller/checkInController.java
+++ src/main/java/com/uday/checkin/controller/checkInController.java
@@ -2,10 +2,13 @@ package com.uday.checkin.controller;
 
 import com.uday.checkin.integration.ReservationRestClient;
 import com.uday.checkin.integration.dto.Reservation;
+import com.uday.checkin.integration.dto.ReservationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -25,5 +28,14 @@ public class checkInController {
         Reservation reservation=reservationRestClient.findReservation(id);
         modelMap.addAttribute("reservation",reservation);
         return "displayReservation";
+    }
+
+    @RequestMapping(value="/updateCheckIn", method = RequestMethod.POST)
+    public String updateCheckIn(@ModelAttribute("reservationRequest")ReservationRequest reservationRequest){
+
+        System.out.println(reservationRequest);
+        Reservation reservation=reservationRestClient.updateReservation(reservationRequest);
+
+        return "reservationSuccess";
     }
 }
